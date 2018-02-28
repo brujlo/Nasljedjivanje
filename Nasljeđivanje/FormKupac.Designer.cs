8717d943cce7d6cead84f7196a7442fe5feb18ee
Bruno Brckovic <bruno.brckovic@gmail.com>
2018-02-28 22:27:57 +0100

    gotovo

--- Nasljeđivanje/FormKupac.Designer.cs
+++ Nasljeđivanje/FormKupac.Designer.cs
@@ -33,81 +33,88 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBoxAdresaDostave = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.buttonOK = new System.Windows.Forms.Button();
+            this.textBoxWithDefault1 = new Nasljeđivanje.TextBoxWithDefault();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Location = new System.Drawing.Point(24, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.Size = new System.Drawing.Size(145, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "&Ime i prezime:";
             // 
             // textBoxImePrezime
             // 
-            this.textBoxImePrezime.Location = new System.Drawing.Point(115, 11);
+            this.textBoxImePrezime.Location = new System.Drawing.Point(230, 21);
+            this.textBoxImePrezime.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBoxImePrezime.Name = "textBoxImePrezime";
-            this.textBoxImePrezime.Size = new System.Drawing.Size(218, 20);
+            this.textBoxImePrezime.Size = new System.Drawing.Size(432, 31);
             this.textBoxImePrezime.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Location = new System.Drawing.Point(24, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.Size = new System.Drawing.Size(168, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "&Adresa dostave:";
             // 
             // textBoxAdresaDostave
             // 
-            this.textBoxAdresaDostave.Location = new System.Drawing.Point(115, 43);
+            this.textBoxAdresaDostave.Location = new System.Drawing.Point(230, 83);
+            this.textBoxAdresaDostave.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.textBoxAdresaDostave.Name = "textBoxAdresaDostave";
-            this.textBoxAdresaDostave.Size = new System.Drawing.Size(218, 20);
+            this.textBoxAdresaDostave.Size = new System.Drawing.Size(432, 31);
             this.textBoxAdresaDostave.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Location = new System.Drawing.Point(24, 150);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.Size = new System.Drawing.Size(158, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "Adresa &računa:";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(115, 75);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(218, 20);
-            this.textBox1.TabIndex = 5;
-            // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(258, 143);
+            this.buttonOK.Location = new System.Drawing.Point(516, 275);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.Size = new System.Drawing.Size(150, 44);
             this.buttonOK.TabIndex = 6;
             this.buttonOK.Text = "OK";
             this.buttonOK.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // textBoxWithDefault1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.textBoxWithDefault1.Location = new System.Drawing.Point(230, 144);
+            this.textBoxWithDefault1.Name = "textBoxWithDefault1";
+            this.textBoxWithDefault1.Size = new System.Drawing.Size(432, 31);
+            this.textBoxWithDefault1.TabIndex = 7;
+            // 
+            // FormKupac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(345, 178);
+            this.ClientSize = new System.Drawing.Size(690, 342);
+            this.Controls.Add(this.textBoxWithDefault1);
             this.Controls.Add(this.buttonOK);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBoxAdresaDostave);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBoxImePrezime);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "FormKupac";
             this.Text = "Podaci o kupcu";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -121,8 +128,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBoxAdresaDostave;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button buttonOK;
+        private TextBoxWithDefault textBoxWithDefault1;
     }
 }
 
---
+++ Nasljeđivanje/TextBoxWithDefault.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nasljeđivanje
+{
+    class TextBoxWithDefault : TextBox
+    {
+        public TextBoxWithDefault()
+        {
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            base.OnPaint(e);
+            if (TextLength == 0)
+                DrawDefaultText(e);
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, true);
+            if (TextLength == 0)
+                Invalidate();
+        }
+
+        private void DrawDefaultText(PaintEventArgs e)
+        {
+            TextRenderer.DrawText(e.Graphics, defaultText, Font, new Point(-2, 1), SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+            //using (Brush brush = new SolidBrush(SystemColors.GrayText))
+            //    e.Graphics.DrawString(defaultText, Font, brush, 0, 0);
+            //ggggg
+
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        private string defaultText = "Podrazumjevana vrijednost";
+    }
+}
