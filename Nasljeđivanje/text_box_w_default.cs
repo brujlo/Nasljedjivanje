7fc9326d46658c6fe727fc3e7a62c05b099e797d
Luka N Sever <lukasever.56@gmail.com>
2018-02-28 20:26:52 +0100

    custom kontrola with default

---
+++ Nasljeđivanje/text_box_w_default.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nasljeđivanje
+{
+    class text_box_w_default : TextBox
+    {
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (TextLength == 0)
+                DrawDefaultText(e);
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+            if (TextLength == 0)
+                Invalidate();
+        }
+
+        private void DrawDefaultText(PaintEventArgs e)
+        {
+            TextRenderer.DrawText(e.Graphics, defaulttext, Font, new Point(-2, 1), SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+            //using(Brush brush = new SolidBrush(SystemColors.GrayText))
+            //    e.Graphics.DrawString(defaulttext, Font, brush, 0, 0);
+        }
+        private string defaulttext = "tvoja adresa";
+    }
+}
